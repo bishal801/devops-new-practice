d2b1e4eb205cd08d303a92d8c894cd616010e8c1
bishal801 <chettribishal801@gmail.com>
2026-09-08 00:45:09 +0530

    commented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetchTasks();
   };
 
-  let sangam = 'unused variable not here';
+  // let sangam = 'unused variable not here';
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Header */}
