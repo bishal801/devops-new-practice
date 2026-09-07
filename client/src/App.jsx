613763969cb4232116c95ebf11cf1786a2c76e67
bishal801 <chettribishal801@gmail.com>
2026-09-08 00:38:51 +0530

    push new changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,8 +60,7 @@ function App() {
     fetchTasks();
   };
 
-  // let sangam = 'unused variable here';
-
+  let sangam = 'unused variable not here';
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Header */}
